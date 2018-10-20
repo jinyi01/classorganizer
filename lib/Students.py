e49a06daf084358e941865dffbe33901d9500a89
zek01 <39113059+zek01@users.noreply.github.com>
2018-10-20 13:27:39 -0700

    Add files via upload

---
+++ lib/Students.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 grade,
+                 gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.grade = grade
+        self.gender = gender
+
+    def __str__(self):
+        return None
