a5448387ce3722971e720b3deff95fdc07372746
Jin Yi <jinyi2001.jl@gmail.com>
2019-03-04 23:11:48 -0800

    updated packages

---
+++ packages/classrooms.py
@@ -0,0 +1,15 @@
+class Division:
+    def __init__(self, grade, size):
+        # ell = false means no special needs assistant, off by default
+        self.grade = grade
+        self.size = size
+        self.ell = False
+        self.student_list = []
+
+    def __str__(self):
+        return "{} ".format(str(self.grade)) + ", ".join([student.first_name for student in self.student_list])
+
+    def add_student(self, student):
+        if len(self.student_list) >= self.size:
+            return student
+        self.student_list.append(student)
